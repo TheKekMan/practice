aa27d37a5a5fe8b7e4a97d294719496a6bf5ad5a
TheKekMan <baxodirovich777@gmail.com>
2021-09-07 17:21:08 +0300

    iteration 12 fix

--- iteration 11 12/src/routes/Async.tsx
+++ iteration 11 12/src/routes/Async.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import Repos from "../components/repos";
 
 class Async extends React.Component {
-  state: any|string
-  setState: any
-  user: string | undefined
+  state: any | string;
+  setState: any;
+  user: string | undefined;
   constructor(props: any) {
     super(props);
     this.setUser = this.setUser.bind(this);
@@ -13,35 +13,39 @@ class Async extends React.Component {
   setUser(user1: string) {
     this.setState({ user: user1 });
   }
-  componentDidUpdate(){
-    if(this.state.user){
+  componentDidUpdate() {
+    if (this.state.user) {
       document.title = `Checking repos of ${this.state.user}`;
-    }
-    else document.title = `Test Async/await`;
+    } else document.title = `Test Async/await`;
   }
-  componentDidMount(){
+  componentDidMount() {
     document.title = `Test Async/await`;
   }
 
   button = async () => {
-    let url: string = `https://api.github.com/users/${this.state.user}/repos`;
-    try {
-      let response: Response = await fetch(url);
-      if (response.status >= 200 && response.status < 300) {
-        let repodata = await response.json();
-        if (repodata.length > 0) {
-          this.setState({ repos: repodata });
+    if (typeof this.state.user !== "string") {
+      let error = new Error("Некорректный формат данных");
+      throw error;
+    } else {
+      let url: string = `https://api.github.com/users/${this.state.user}/repos`;
+      try {
+        let response: Response = await fetch(url);
+        if (response.status >= 200 && response.status < 300) {
+          let repodata = await response.json();
+          if (repodata.length > 0 && typeof repodata !== 'undefined') {
+            this.setState({ repos: repodata });
+          } else {
+            let error = new Error("Репозитории отсутствуют");
+            throw error;
+          }
         } else {
-          let error = new Error("Репозитории отсутствуют");
+          let error = new Error(response.status + " " + response.statusText);
           throw error;
         }
-      } else {
-        let error = new Error(response.status + " " + response.statusText);
-        throw error;
+      } catch (err: any) {
+        alert("Error: " + err.message);
+        this.setState({ repos: [] });
       }
-    } catch (err: any) {
-      alert("Error: " + err.message);
-      this.setState({ repos: [] });
     }
   };
   render() {
@@ -61,15 +65,23 @@ class Async extends React.Component {
           </button>
           <h3>Репозитории пользователя:</h3>
           <div id="repos">
-            {this.state.repos.map((repos: { id: React.Key | null | undefined; name: any; created_at: string | number | Date; description: string; html_url: any; }) => (
-              <Repos
-                key={repos.id}
-                name={repos.name}
-                createdAt={new Date(repos.created_at).toLocaleDateString()}
-                description={repos.description}
-                url={repos.html_url}
-              />
-            ))}
+            {this.state.repos.map(
+              (repos: {
+                id: React.Key | null | undefined;
+                name: any;
+                created_at: string | number | Date;
+                description: string;
+                html_url: any;
+              }) => (
+                <Repos
+                  key={repos.id}
+                  name={repos.name}
+                  createdAt={new Date(repos.created_at).toLocaleDateString()}
+                  description={repos.description}
+                  url={repos.html_url}
+                />
+              )
+            )}
           </div>
         </section>
         <hr />
