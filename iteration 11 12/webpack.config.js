d752a7b031758c282390289d1c1eedfdfd7e1b88
TheKekMan <baxodirovich777@gmail.com>
2021-09-08 11:28:19 +0300

    iteration 12 webpack fix

--- iteration 11 12/webpack.config.js
+++ iteration 11 12/webpack.config.js
@@ -8,13 +8,6 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    compress: true,
-    port: 9000,
-  },
   plugins: [
     new HTMLWebpackPlugin({
       template: "./public/index.html",
