2eda1da43f59e8a981a6a773c707bc85c53dad66
TheKekMan <baxodirovich777@gmail.com>
2021-09-08 11:20:07 +0300

    iteration 12 webpack

--- iteration 11 12/webpack.config.js
+++ iteration 11 12/webpack.config.js
@@ -1,30 +1,62 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './built/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './public/index.html'
-        })
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              [
+                "@babel/preset-react",
+                {
+                  runtime: "automatic",
+                },
+              ],
+            ],
+          },
+        },
+      },
+      { test: /\.(css)$/, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(tsx)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              [
+                "@babel/preset-typescript",
+                {
+                  runtime: "automatic",
+                },
+              ],
+            ],
+          },
+        },
+      },
     ],
-    module: {
-        rules: [
-            { test: /\.(js)$/, 
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ["@babel/preset-env", ["@babel/preset-react", {
-                        "runtime": "automatic"
-                     }]]
-                }
-            }},
-            { test: /\.(css)$/, use: ['style-loader', 'css-loader']}
-        ]
-    }
-}
+  },
+};
